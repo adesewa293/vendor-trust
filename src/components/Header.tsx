9b56354e637c14c1b30f80ac4fe1979e89dacbeb
Adesewa Aishat <adesewaaishat@gmail.com>
2024-01-10 19:49:43 +0000

    add base informations

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+
+
+export default function Header() {
+  return (
+    <div>
+      <div className="flex justify-between p-10">
+     <div>VendorTrust</div>
+     <div>
+      <ul className="flex justify-between">
+        <li>categories</li>
+        <li>Blog</li>
+        <li>Log in</li>
+        <li>For Businesses</li>
+      </ul>
+     </div>
+     </div>
+    </div>
+  )
+}
